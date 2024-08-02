92f0bb2826e036fcc2a6eb520394c256670d0972
Amine S <amine.s3y@gmail.com>
2024-08-02 07:51:07 +0200

    Revert "feat: add a reaction on page"
    This reverts commit 5831196cc44eb84a9e0603850ab5fa44784c729d.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
-import { ReactionForm } from "@/components/ReactionForm";
-
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`relative ${inter.className}`}>
-        {children}
-        <ReactionForm />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
